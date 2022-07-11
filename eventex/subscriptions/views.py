9e282c782c9acfa01b8c9cb7170cb73dafb7c012
mathpereira1 <matheusps3110@gmail.com>
2022-07-11 14:31:59 -0300

    From alinhado com o Sender Identity

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -15,8 +15,8 @@ def subscribe(request):
             body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
             mail.send_mail('Confirmação de inscrição',
                             body,
-                        'contato@eventex.com.br',
-                            ['contato@eventex.com.br', form.cleaned_data['email']])
+                            'matheusps3110@outlook.com',
+                            ['matheusps3110@outlook.com', form.cleaned_data['email']])
             messages.success(request, 'Inscrição realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
         else:

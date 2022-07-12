227ebdb7656a433145c9f6e3cbdf52eb9316d7bd
mathpereira1 <matheusps3110@gmail.com>
2022-07-12 10:54:16 -0300

    general changes

---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,36 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Matheus Pereira', cpf='12345678901',
+            email='matheusps3110@gmail.com', phone='11-98322-2522')
+        self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+        
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+        
+        self.assertEqual(expect, self.email.subject)
+        
+    def test_subscription_email_from(self):
+        expect = 'matheusps3110@outlook.com'
+        
+        self.assertEqual(expect, self.email.from_email)
+        
+    def test_subscription_email_to(self):
+        expect = ['matheusps3110@outlook.com', 'matheusps3110@gmail.com']
+        
+        self.assertEqual(expect, self.email.to)
+        
+    def test_subscription_email_body(self):
+        contents = [ 
+            'Matheus Pereira',
+            '12345678901',
+            'matheusps3110@gmail.com',
+            '11-98322-2522',
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)

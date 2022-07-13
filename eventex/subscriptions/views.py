d4a5096db6b499e8c3e1c0245d90d42066af06d1
mathpereira1 <88118960+mathpereira1@users.noreply.github.com>
2022-07-13 11:39:55 -0300

    Persiste a inscrição no DB

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseRedirect
 from django.template.loader import render_to_string
 
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 def subscribe(request):
     if request.method == 'POST':
@@ -29,7 +30,7 @@ def create(request):
                'subscriptions/subscription_email.txt',
                form.cleaned_data)
     
-
+    Subscription.objects.create(**form.cleaned_data)
     # Success feedback
     messages.success(request, 'Inscrição realizada com sucesso!')
     return HttpResponseRedirect('/inscricao/')
